39350d41fca3dee421b33037fa492f93a3731f3c
Nebemires <Orionas3@gmail.com>
2020-08-19 09:58:36 +0300

    Update wol.sh

--- wol.sh
+++ wol.sh
@@ -1,31 +1,47 @@
-powerwake 70:85:c2:73:62:4f       #K14
+#K14
+powerwake 70:85:c2:73:62:4f       
 sleep 5
-                        	        #K15
-powerwake 70:85:c2:4b:8e:69       #K16
+
+#K16                     	  
+powerwake 70:85:c2:4b:8e:69       
 sleep 5
-powerwake 70:85:c2:4b:92:8f       #K17
+
+#K17
+powerwake 70:85:c2:4b:92:8f       
 sleep 5
-                        	        #K18
-powerwake 70:85:c2:69:7f:77       #K19
+#K19                       	  
+powerwake 70:85:c2:69:7f:77       
 sleep 5
-powerwake 70:85:c2:75:05:d3       #K20
+
+#K20
+powerwake 70:85:c2:75:05:d3       
 sleep 5
-powerwake 70:85:c2:75:04:0b       #K21
+
+#K21
+powerwake 70:85:c2:75:04:0b       
 sleep 5
-powerwake 70:85:c2:69:7b:5f       #K22
+
+#K22
+powerwake 70:85:c2:69:7b:5f       
 sleep 5
-powerwake 70:85:c2:75:08:7b       #K23
+
+#K23
+powerwake 70:85:c2:75:08:7b       
 sleep 5
-powerwake 70:85:c2:67:04:8f       #K24
+
+#K24
+powerwake 70:85:c2:67:04:8f      
 sleep 5
-                        	        #K25
-         			                    #K26
-powerwake 70:85:c2:75:ba:37       #K27
+
+#K27                      	  
+powerwake 70:85:c2:75:ba:37       
 sleep 5
-                        	        #K28
-powerwake 70:85:c2:4b:98:0c       #K29
+
+#K29                 	  
+powerwake 70:85:c2:4b:98:0c       
 sleep 5
-                        	        #K30
-                        	        #K31
-powerwake 70:85:c2:4b:98:36       #K32
+
+#K32                       	  
+powerwake 70:85:c2:4b:98:36
+     
 echo "All Done!"
