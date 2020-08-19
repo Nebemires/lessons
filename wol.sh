c9917465c17679aeded1c7f1bfa0c2216febd941
Nebemires <Orionas3@gmail.com>
2020-08-19 09:55:10 +0300

    Update wol.sh

--- wol.sh
+++ wol.sh
@@ -1,21 +1,31 @@
-powerwake 70:85:c2:73:62:4f      #K14
-                        	#K15
+powerwake 70:85:c2:73:62:4f       #K14
+sleep 5
+                        	        #K15
 powerwake 70:85:c2:4b:8e:69       #K16
+sleep 5
 powerwake 70:85:c2:4b:92:8f       #K17
-                        	#K18
+sleep 5
+                        	        #K18
 powerwake 70:85:c2:69:7f:77       #K19
+sleep 5
 powerwake 70:85:c2:75:05:d3       #K20
+sleep 5
 powerwake 70:85:c2:75:04:0b       #K21
+sleep 5
 powerwake 70:85:c2:69:7b:5f       #K22
+sleep 5
 powerwake 70:85:c2:75:08:7b       #K23
+sleep 5
 powerwake 70:85:c2:67:04:8f       #K24
-                        	#K25
-                       	 	#K26
+sleep 5
+                        	        #K25
+         			                    #K26
 powerwake 70:85:c2:75:ba:37       #K27
-                        	#K28
+sleep 5
+                        	        #K28
 powerwake 70:85:c2:4b:98:0c       #K29
-                        	#K30
-                        	#K31
+sleep 5
+                        	        #K30
+                        	        #K31
 powerwake 70:85:c2:4b:98:36       #K32
-                        	#K33
-                        	#K34
+echo "All Done!"
