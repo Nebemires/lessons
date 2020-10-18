1b9eabd2329304e2dcbb3cf09717f78d209664f5
Nebemires <Orionas3@gmail.com>
2020-10-18 16:56:26 +0300

    Update klem2_wol.sh

--- klem2_wol.sh
+++ klem2_wol.sh
@@ -1,15 +1,19 @@
 #K01
 powerwake 70:85:c2:75:06:db
 sleep 5
+
 #K02
 powerwake 70:85:c2:75:06:b1
 sleep 5
+
 #K03
 powerwake 70:85:c2:73:61:c9
 sleep 5
+
 #K04
 powerwake 70:85:c2:73:63:e2
 sleep 5
+
 #K05
 powerwake 70:85:c2:73:62:97
 sleep 5
@@ -19,88 +23,116 @@ sleep 5
 #K07
 powerwake 70:85:c2:75:03:f9
 sleep 5
+
 #K08
 powerwake 70:85:c2:75:0a:b3
 sleep 5
+
 #K09
 powerwake 70:85:c2:75:06:17
 sleep 5
+
 #K10
 powerwake 70:85:c2:75:05:b7
 sleep 5
+
 #K11
 powerwake 70:85:c2:75:03:fd
 sleep 5
+
 #K12
 powerwake 70:85:c2:75:03:f7
 sleep 5
+
 #K13
 powerwake 70:85:c2:4b:88:49
 sleep 5
+
 #K14
 powerwake 70:85:c2:73:62:4f
 sleep 5
+
 #K15
 powerwake 70:85:c2:75:06:1f
 sleep 5
+
 #K16
 powerwake 70:85:c2:4b:8e:69
 sleep 5
+
 #K17
 powerwake 70:85:c2:4b:92:8f
 sleep 5
+
 #K18
 powerwake 70:85:c2:4b:97:e8
 sleep 5
 #K19
 powerwake 70:85:c2:69:7f:77
 sleep 5
+
 #K20
 powerwake 70:85:c2:75:05:d3
 sleep 5
+
 #K21
 powerwake 70:85:c2:75:04:0b
 sleep 5
+
 #K22
 powerwake 70:85:c2:69:7b:5f
 sleep 5
+
 #K23
 powerwake 70:85:c2:75:08:7b
 sleep 5
+
 #K24
 powerwake 70:85:c2:67:04:8f
 sleep 5
+
 #K25
 powerwake 70:85:c2:73:62:79
 sleep 5
+
 #K26
 powerwake 70:85:c2:4b:56:f6
 sleep 5
+
 #K27
 powerwake 70:85:c2:75:ba:37
 sleep 5
+
 #K28
 powerwake 70:85:c2:4b:78:98
 sleep 5
+
 #K29
 powerwake 70:85:c2:4b:98:0c
 sleep 5
+
 #K30
 powerwake 70:85:c2:4b:98:f5
 sleep 5
+
 #K31
 powerwake 70:85:c2:4b:98:2c
 sleep 5
+
 #K32
 powerwake 70:85:c2:4b:98:36
 sleep 5
+
 #K33
 powerwake 70:85:c2:4b:98:10
 sleep 5
+
 #K34
 powerwake 70:85:c2:75:0a:a1
 sleep 5
+
 #K36
 powerwake 70:85:c2:4f:de:ac
 sleep 5
+
 echo "All Done!"
