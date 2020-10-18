c670ec982c98db4c6fe83fede292c9a2cdd88580
Nebemires <Orionas3@gmail.com>
2020-10-18 16:35:29 +0300

    Update klem2_wol.sh

--- klem2_wol.sh
+++ klem2_wol.sh
@@ -1,106 +1,106 @@
 #K01
-70:85:c2:75:06:db
+powerwake 70:85:c2:75:06:db
 sleep 5
 #K02
-70:85:c2:75:06:b1
+powerwake 70:85:c2:75:06:b1
 sleep 5
 #K03
-70:85:c2:73:61:c9
+powerwake 70:85:c2:73:61:c9
 sleep 5
 #K04
-70:85:c2:73:63:e2 
+powerwake 70:85:c2:73:63:e2 
 sleep 5
 #K05
-70:85:c2:73:62:97
+powerwake 70:85:c2:73:62:97
 sleep 5
 #K06
-70:85:c2:75:04:83 
+powerwake 70:85:c2:75:04:83 
 sleep 5
 #K07
-70:85:c2:75:03:f9
+powerwake 70:85:c2:75:03:f9
 sleep 5
 #K08
-70:85:c2:75:0a:b3
+powerwake 70:85:c2:75:0a:b3
 sleep 5
 #K09
-70:85:c2:75:06:17
+powerwake 70:85:c2:75:06:17
 sleep 5
 #K10
-70:85:c2:75:05:b7
+powerwake 70:85:c2:75:05:b7
 sleep 5
 #K11
-70:85:c2:75:03:fd
+powerwake 70:85:c2:75:03:fd
 sleep 5
 #K12
-70:85:c2:75:03:f7
+powerwake 70:85:c2:75:03:f7
 sleep 5
 #K13
-70:85:c2:4b:88:49
+powerwake 70:85:c2:4b:88:49
 sleep 5
 #K14
-70:85:c2:73:62:4f
+powerwake 70:85:c2:73:62:4f
 sleep 5
 #K15
-70:85:c2:75:06:1f
+powerwake 70:85:c2:75:06:1f
 sleep 5
 #K16
-70:85:c2:4b:8e:69
+powerwake 70:85:c2:4b:8e:69
 sleep 5
 #K17
-70:85:c2:4b:92:8f
+powerwake 70:85:c2:4b:92:8f
 sleep 5
 #K18
-70:85:c2:4b:97:e8
+powerwake 70:85:c2:4b:97:e8
 sleep 5
 #K19
-70:85:c2:69:7f:77
+powerwake 70:85:c2:69:7f:77
 sleep 5
 #K20
-70:85:c2:75:05:d3
+powerwake 70:85:c2:75:05:d3
 sleep 5
 #K21
-70:85:c2:75:04:0b
+powerwake 70:85:c2:75:04:0b
 sleep 5
 #K22
-70:85:c2:69:7b:5f
+powerwake 70:85:c2:69:7b:5f
 sleep 5
 #K23
-70:85:c2:75:08:7b
+powerwake 70:85:c2:75:08:7b
 sleep 5
 #K24
-70:85:c2:67:04:8f
+powerwake 70:85:c2:67:04:8f
 sleep 5
 #K25
-70:85:c2:73:62:79
+powerwake 70:85:c2:73:62:79
 sleep 5
 #K26
-70:85:c2:4b:56:f6
+powerwake 70:85:c2:4b:56:f6
 sleep 5
 #K27
-70:85:c2:75:ba:37
+powerwake 70:85:c2:75:ba:37
 sleep 5
 #K28
-70:85:c2:4b:78:98
+powerwake 70:85:c2:4b:78:98
 sleep 5
 #K29
-70:85:c2:4b:98:0c
+powerwake 70:85:c2:4b:98:0c
 sleep 5
 #K30
-70:85:c2:4b:98:f5
+powerwake 70:85:c2:4b:98:f5
 sleep 5
 #K31
-70:85:c2:4b:98:2c
+powerwake 70:85:c2:4b:98:2c
 sleep 5
 #K32
-70:85:c2:4b:98:36
+powerwake 70:85:c2:4b:98:36
 sleep 5
 #K33
-70:85:c2:4b:98:10
+powerwake 70:85:c2:4b:98:10
 sleep 5
 #K34
-70:85:c2:75:0a:a1
+powerwake 70:85:c2:75:0a:a1
 sleep 5
 #K36
-70:85:c2:4f:de:ac
+powerwake 70:85:c2:4f:de:ac
 sleep 5
 echo "All Done!"
